747bc7a615ab04a50b8657fc6ab635ceef820bcb
Brad Parbs <brad@bradparbs.com>
2023-03-17 12:39:39 -0500

    Update lint

--- mh-catch-stats.user.js
+++ mh-catch-stats.user.js
@@ -13,177 +13,177 @@
 // ==/UserScript==
 
 ((function () {
-	'use strict';
+  'use strict';
 
-	/**
-	 * Get the mouse stats.
-	 *
-	 * @return {Object} The mouse stats.
-	 */
-	const getMouseStats = async () => {
-		const data = await doRequest(
-			'managers/ajax/mice/mouse_list.php',
-			{
-				action: 'get_environment',
-				category: user.environment_type, // eslint-disable-line no-undef
-				user_id: user.user_id, // eslint-disable-line no-undef
-				display_mode: 'stats',
-				view: 'ViewMouseListEnvironments',
-			}
-		);
+  /**
+   * Get the mouse stats.
+   *
+   * @return {Object} The mouse stats.
+   */
+  const getMouseStats = async () => {
+    const data = await doRequest(
+      'managers/ajax/mice/mouse_list.php',
+      {
+        action: 'get_environment',
+        category: user.environment_type, // eslint-disable-line no-undef
+        user_id: user.user_id, // eslint-disable-line no-undef
+        display_mode: 'stats',
+        view: 'ViewMouseListEnvironments',
+      }
+    );
 
-		// Grab the data from the response.
-		const mouseData = data?.mouse_list_category?.subgroups[ 0 ]?.mice;
+    // Grab the data from the response.
+    const mouseData = data?.mouse_list_category?.subgroups[ 0 ]?.mice;
 
-		// Reorder by the num_catches key.
-		mouseData.sort((a, b) => {
-			return b.num_catches - a.num_catches;
-		});
+    // Reorder by the num_catches key.
+    mouseData.sort((a, b) => {
+      return b.num_catches - a.num_catches;
+    });
 
-		// Return the data.
-		return mouseData ? mouseData : [];
-	};
+    // Return the data.
+    return mouseData ? mouseData : [];
+  };
 
-	/**
-	 * Build the markup for the stats.
-	 *
-	 * @param {Object} mouseData The mouse data.
-	 *
-	 * @return {Node} The node to append.
-	 */
-	const buildMouseMarkup = (mouseData) => {
-		// Fallbacks for mouse data.
-		const mouse = Object.assign({}, {
-			name: '',
-			type: '',
-			image: '',
-			crown: 'none',
-			num_catches: 0,
-		}, mouseData);
+  /**
+   * Build the markup for the stats.
+   *
+   * @param {Object} mouseData The mouse data.
+   *
+   * @return {Node} The node to append.
+   */
+  const buildMouseMarkup = (mouseData) => {
+    // Fallbacks for mouse data.
+    const mouse = Object.assign({}, {
+      name: '',
+      type: '',
+      image: '',
+      crown: 'none',
+      num_catches: 0,
+    }, mouseData);
 
-		const mouseEl = document.createElement('a');
-		mouseEl.classList.add('mh-catch-stats');
+    const mouseEl = document.createElement('a');
+    mouseEl.classList.add('mh-catch-stats');
 
-		mouseEl.title = mouse.name;
-		mouseEl.addEventListener('click', () => {
-			if ('undefined' !== hg?.views?.MouseView?.show) { // eslint-disable-line no-undef
-				hg.views.MouseView.show(mouse.type); // eslint-disable-line no-undef
-			}
-		});
+    mouseEl.title = mouse.name;
+    mouseEl.addEventListener('click', () => {
+      if ('undefined' !== hg?.views?.MouseView?.show) { // eslint-disable-line no-undef
+        hg.views.MouseView.show(mouse.type); // eslint-disable-line no-undef
+      }
+    });
 
-		// Create the image element.
-		const image = document.createElement('div');
-		image.classList.add('mh-catch-stats-image');
-		image.style.backgroundImage = `url('${ mouse.image }')`;
+    // Create the image element.
+    const image = document.createElement('div');
+    image.classList.add('mh-catch-stats-image');
+    image.style.backgroundImage = `url('${ mouse.image }')`;
 
-		// If the mouse has a crown, add it.
-		if (mouse.crown && 'none' !== mouse.crown) {
-			const crown = document.createElement('div');
-			crown.classList.add('mh-catch-stats-crown');
-			crown.style.backgroundImage = `url('https://www.mousehuntgame.com/images/ui/crowns/crown_${ mouse.crown }.png')`;
-			image.appendChild(crown);
-		}
+    // If the mouse has a crown, add it.
+    if (mouse.crown && 'none' !== mouse.crown) {
+      const crown = document.createElement('div');
+      crown.classList.add('mh-catch-stats-crown');
+      crown.style.backgroundImage = `url('https://www.mousehuntgame.com/images/ui/crowns/crown_${ mouse.crown }.png')`;
+      image.appendChild(crown);
+    }
 
-		// Create the name element.
-		const name = document.createElement('div');
-		name.classList.add('mh-catch-stats-name');
-		name.innerText = mouse.name;
+    // Create the name element.
+    const name = document.createElement('div');
+    name.classList.add('mh-catch-stats-name');
+    name.innerText = mouse.name;
 
-		// Create a wrapper for the name and image.
-		const imageNameContainer = document.createElement('div');
-		imageNameContainer.appendChild(image);
-		imageNameContainer.appendChild(name);
+    // Create a wrapper for the name and image.
+    const imageNameContainer = document.createElement('div');
+    imageNameContainer.appendChild(image);
+    imageNameContainer.appendChild(name);
 
-		// Create the catches element.
-		const catches = document.createElement('div');
-		catches.classList.add('mh-catch-stats-catches');
-		catches.innerText = mouse.num_catches;
+    // Create the catches element.
+    const catches = document.createElement('div');
+    catches.classList.add('mh-catch-stats-catches');
+    catches.innerText = mouse.num_catches;
 
-		mouseEl.appendChild(imageNameContainer);
-		mouseEl.appendChild(catches);
+    mouseEl.appendChild(imageNameContainer);
+    mouseEl.appendChild(catches);
 
-		return mouseEl;
-	};
+    return mouseEl;
+  };
 
-	/**
-	 * Show the stat modal.
-	 */
-	const showModal = async () => {
-		// Remove the existing modal.
-		const existing = document.getElementById('mh-catch-stats');
-		if (existing) {
-			existing.remove();
-		}
+  /**
+   * Show the stat modal.
+   */
+  const showModal = async () => {
+    // Remove the existing modal.
+    const existing = document.getElementById('mh-catch-stats');
+    if (existing) {
+      existing.remove();
+    }
 
-		// Create the modal.
-		const modalWrapper = document.createElement('div');
-		modalWrapper.id = 'mh-catch-stats';
+    // Create the modal.
+    const modalWrapper = document.createElement('div');
+    modalWrapper.id = 'mh-catch-stats';
 
-		// Create the wrapper.
-		const modal = document.createElement('div');
-		modal.classList.add('mh-catch-stats-wrapper');
+    // Create the wrapper.
+    const modal = document.createElement('div');
+    modal.classList.add('mh-catch-stats-wrapper');
 
-		// Create the header.
-		const header = document.createElement('div');
-		header.classList.add('mh-catch-stats-header');
+    // Create the header.
+    const header = document.createElement('div');
+    header.classList.add('mh-catch-stats-header');
 
-		// Add the title;
-		const title = document.createElement('h1');
-		title.innerText = 'Mouse Catch Stats';
-		header.appendChild(title);
+    // Add the title;
+    const title = document.createElement('h1');
+    title.innerText = 'Mouse Catch Stats';
+    header.appendChild(title);
 
-		// Create a close button icon.
-		const closeIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-		closeIcon.classList.add('mh-catch-stats-close');
-		closeIcon.setAttribute('viewBox', '0 0 24 24');
-		closeIcon.setAttribute('width', '18');
-		closeIcon.setAttribute('height', '18');
-		closeIcon.setAttribute('fill', 'none');
-		closeIcon.setAttribute('stroke', 'currentColor');
-		closeIcon.setAttribute('stroke-width', '1.5');
+    // Create a close button icon.
+    const closeIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    closeIcon.classList.add('mh-catch-stats-close');
+    closeIcon.setAttribute('viewBox', '0 0 24 24');
+    closeIcon.setAttribute('width', '18');
+    closeIcon.setAttribute('height', '18');
+    closeIcon.setAttribute('fill', 'none');
+    closeIcon.setAttribute('stroke', 'currentColor');
+    closeIcon.setAttribute('stroke-width', '1.5');
 
-		// Create the path.
-		const closePath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-		closePath.setAttribute('d', 'M18 6L6 18M6 6l12 12');
-		closeIcon.appendChild(closePath);
+    // Create the path.
+    const closePath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    closePath.setAttribute('d', 'M18 6L6 18M6 6l12 12');
+    closeIcon.appendChild(closePath);
 
-		// Close the modal when the icon is clicked.
-		closeIcon.addEventListener('click', () => {
-			modalWrapper.remove();
-		});
+    // Close the modal when the icon is clicked.
+    closeIcon.addEventListener('click', () => {
+      modalWrapper.remove();
+    });
 
-		// Append the button.
-		header.appendChild(closeIcon);
+    // Append the button.
+    header.appendChild(closeIcon);
 
-		// Add the header to the modal.
-		modal.appendChild(header);
+    // Add the header to the modal.
+    modal.appendChild(header);
 
-		// Make the mouse stats table.
-		const mouseBody = document.createElement('div');
-		mouseBody.classList.add('mh-catch-stats-body');
+    // Make the mouse stats table.
+    const mouseBody = document.createElement('div');
+    mouseBody.classList.add('mh-catch-stats-body');
 
-		// Get the mouse stats.
-		const mouseStats = await getMouseStats();
+    // Get the mouse stats.
+    const mouseStats = await getMouseStats();
 
-		// Loop through the stats and add them to the modal.
-		mouseStats.forEach((mouseData) => {
-			mouseBody.appendChild(buildMouseMarkup(mouseData, mouseBody));
-		});
+    // Loop through the stats and add them to the modal.
+    mouseStats.forEach((mouseData) => {
+      mouseBody.appendChild(buildMouseMarkup(mouseData, mouseBody));
+    });
 
-		// Add the mouse stats to the modal.
-		modal.appendChild(mouseBody);
+    // Add the mouse stats to the modal.
+    modal.appendChild(mouseBody);
 
-		// Add the modal to the wrapper.
-		modalWrapper.appendChild(modal);
+    // Add the modal to the wrapper.
+    modalWrapper.appendChild(modal);
 
-		// Add the wrapper to the body.
-		document.body.appendChild(modalWrapper);
+    // Add the wrapper to the body.
+    document.body.appendChild(modalWrapper);
 
-		// Make the modal draggable.
-		makeElementDraggable('#mh-catch-stats', '.mh-catch-stats-header', 25, 25, 'mh-catch-stats-position');
-	};
+    // Make the modal draggable.
+    makeElementDraggable('#mh-catch-stats', '.mh-catch-stats-header', 25, 25, 'mh-catch-stats-position');
+  };
 
-	addStyles(`#mh-catch-stats {
+  addStyles(`#mh-catch-stats {
 			position: absolute;
 			top: 25px;
 			left: 25px;
@@ -292,10 +292,10 @@
 		}
 	`);
 
-	addSubmenuItem({
-		menu: 'mice',
-		label: 'Location Catch Stats',
-		icon: 'https://www.mousehuntgame.com/images/ui/hud/menu/prize_shoppe.png?',
-		callback: showModal
-	});
+  addSubmenuItem({
+    menu: 'mice',
+    label: 'Location Catch Stats',
+    icon: 'https://www.mousehuntgame.com/images/ui/hud/menu/prize_shoppe.png?',
+    callback: showModal
+  });
 })());
